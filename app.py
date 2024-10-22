501a9146cfed56639044b264f33fb64bea5dae56
Naimat <niamatu250@gmail.com>
2024-10-22 10:06:13 +0500

    Function updated

--- app.py
+++ app.py
@@ -1,28 +1,16 @@
-
-import boto3
 import json
-import os
 
-print('Loading function')
+# import requests
 
-#Create the client outside the handler
-region_name=os.environ['REGION_NAME']
-dynamo = boto3.client('dynamodb', region_name=region_name)
-table_name = os.environ['TABLE_NAME']
-
-def respond(err, res=None):
-    return {
-        'statusCode': '400' if err else '200',
-        'body': err.message if err else json.dumps(res),
-        'headers': {
-            'Content-Type': 'application/json',
-        },
-    }
 
 def lambda_handler(event, context):
-    print ("Hello from SA: V1")
-    print("Received event: " + json.dumps(event, indent=2))
-    scan_result=dynamo.scan(TableName=table_name)
-    return respond(None, res=scan_result)
-
-   
+    
+    return {
+        "statusCode": 200,
+        "body": json.dumps(
+            {
+                "message": "hello world SAM",
+                # "location": ip.text.replace("\n", "")
+            }
+        ),
+    }
